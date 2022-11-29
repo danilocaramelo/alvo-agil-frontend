8569811245629e88cd0c95a9532a85ce83d68557
Danilo Caramelo <danilo.caramelo@nomadglobal.com>
2022-11-28 23:45:06 -0300

    feat: new changes in team page

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Col, List, Row, Select, Skeleton, Tooltip, Typography } from 'antd';
+import { Avatar, Col, List, Popover, Row, Select, Skeleton, Tooltip, Typography } from 'antd';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import {
   TeamOutlined,
@@ -7,13 +7,14 @@ import {
   ToolOutlined,
   AimOutlined,
   UserOutlined,
+  DeleteOutlined,
 } from '@ant-design/icons';
 import { AgilWheel, ParticipantDrawer } from '../../containers';
 import { useCallback, useEffect, useState } from 'react';
 import './style.scss';
 import { data2 } from '../../containers/General/AgilWheel/datamock_copy';
 import { useParams } from 'react-router-dom';
-import { getTeam, Team as TeamEntity } from '../../connections/team';
+import { getTeam, Team as TeamEntity, updateTeam } from '../../connections/team';
 import { CustomButton } from '../../components';
 import { AddParticipantModal } from './AddParticipantModal';
 const { Title, Text } = Typography;
@@ -26,7 +27,8 @@ export function Team() {
   const [loadingTeam, setLoadingTeam] = useState<boolean>(false);
   const [addParticipantModalVisible, setAddParticipantModalVisible] = useState<boolean>(false);
 
-  const secondList = teamData?.tecnologias;
+  const teamTechnologies = teamData?.tecnologias;
+  const teamCeremonies = teamData?.cerimonias;
 
   const closeAddParticipantModal = useCallback(() => setAddParticipantModalVisible(false), []);
   const openAddParticipantModal = useCallback(() => {
@@ -47,9 +49,23 @@ export function Team() {
   const closeParticipantDrawer = useCallback(() => setShowParticipantDrawer(false), []);
   const openParticipantDrawer = useCallback(() => setShowParticipantDrawer(true), []);
 
+  const removeTeamParticipant = useCallback(
+    async (deleteId: number) => {
+      const newParticipants = teamData?.participantes?.filter(
+        (participant) => participant.cdParticipante !== deleteId,
+      );
+      const newTeam: TeamEntity = {
+        ...teamData!,
+        participantes: newParticipants ? [...newParticipants] : [],
+      };
+      await updateTeam(newTeam);
+    },
+    [teamData],
+  );
+
   return (
     <div className='team-page'>
-      <Row style={{ marginBottom: 50 }}>
+      <Row>
         <Col span={8}>
           <Row justify='center'>
             <Title level={2} className='team-name'>
@@ -57,7 +73,21 @@ export function Team() {
             </Title>
           </Row>
         </Col>
-        <Col span={12}>
+      </Row>
+      <Row>
+        <Col span={8}>
+          <Row justify='center'>
+            <CustomButton label='Criar nova avaliação' />
+          </Row>
+          <Row justify='center' style={{ marginTop: 20 }}>
+            <Select style={{ width: 120 }} defaultValue='1'>
+              <Select.Option value='1'>17/09/2022</Select.Option>
+              <Select.Option value='2'>23/10/2022</Select.Option>
+            </Select>
+          </Row>
+          <AgilWheel data={data2} />
+        </Col>
+        <Col span={8}>
           <Row style={{ marginBottom: 10 }}>
             <Col className='team-description-item' span={8}>
               <Tooltip title='Nº de pessoas'>
@@ -102,65 +132,81 @@ export function Team() {
               </Tooltip>
             </Col>
           </Row>
+          <List
+            header={<div>Tecnologias</div>}
+            bordered
+            dataSource={teamTechnologies}
+            renderItem={(item) => <List.Item>{item?.nmTecnologia}</List.Item>}
+            className='list'
+            locale={{ emptyText: '' }}
+          />
+          <List
+            header={<div>Cerimônias</div>}
+            bordered
+            dataSource={teamCeremonies}
+            renderItem={(item) => <List.Item>{item?.nmCerimonia}</List.Item>}
+            className='list'
+          />
         </Col>
-      </Row>
-      <Row align='middle'>
-        <Col span={8}>
-          <Row justify='center'>
-            <Select style={{ width: 120 }} defaultValue='1'>
-              <Select.Option value='1'>17/09/2022</Select.Option>
-              <Select.Option value='2'>23/10/2022</Select.Option>
-            </Select>
+        <Col span={6} offset={1}>
+          <Row justify='center' style={{ marginBottom: 20 }}>
+            <CustomButton label='Adicionar participante' onClick={openAddParticipantModal} />
           </Row>
-          <AgilWheel data={data2} />
-        </Col>
-        <Col span={12}>
-          <Row>
-            <Col span={12}>
-              <Row justify='center' style={{ marginBottom: 20 }}>
-                <CustomButton label='Adicionar participante' onClick={openAddParticipantModal} />
-              </Row>
-              <div
-                id='scrollableDiv'
-                style={{
-                  height: '350px',
-                  overflow: 'auto',
-                  padding: '0 16px',
-                  backgroundColor: '#fff',
-                }}
-              >
-                <InfiniteScroll
-                  dataLength={teamData?.participantes?.length ?? 0}
-                  next={() => {
-                    console.log();
-                  }}
-                  hasMore={loadingTeam}
-                  loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
-                  scrollableTarget='scrollableDiv'
-                >
-                  <List
-                    dataSource={teamData?.participantes}
-                    renderItem={(item) => (
-                      <List.Item key={item.emailParticipante}>
-                        <List.Item.Meta
-                          avatar={<Avatar icon={<UserOutlined />} />}
-                          title={<a onClick={openParticipantDrawer}>{item.nmParticipante}</a>}
-                          description={item.emailParticipante}
-                        />
-                      </List.Item>
-                    )}
-                  />
-                </InfiniteScroll>
-              </div>
-            </Col>
-            <Col span={12}>
+          <div
+            id='scrollableDiv'
+            style={{
+              height: '400px',
+              overflow: 'auto',
+              padding: '0 16px',
+              backgroundColor: '#fff',
+              borderRadius: '10px',
+            }}
+          >
+            <InfiniteScroll
+              dataLength={teamData?.participantes?.length ?? 0}
+              next={() => {
+                console.log();
+              }}
+              hasMore={loadingTeam}
+              loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+              scrollableTarget='scrollableDiv'
+            >
               <List
-                bordered
-                dataSource={secondList}
-                renderItem={(item) => <List.Item>{item.nmTecnologia}</List.Item>}
+                dataSource={teamData?.participantes}
+                renderItem={(item) => (
+                  <List.Item key={item.emailParticipante}>
+                    <List.Item.Meta
+                      avatar={<Avatar icon={<UserOutlined />} />}
+                      title={<a onClick={openParticipantDrawer}>{item.nmParticipante}</a>}
+                      description={
+                        <Row justify='space-between'>
+                          <div>{item.emailParticipante}</div>
+                          <Popover
+                            content={
+                              <>
+                                <div>Tem certeza que deseja excluir?</div>
+                                <Row justify='center'>
+                                  <CustomButton
+                                    onClick={() => removeTeamParticipant(item.cdParticipante)}
+                                    label='Confirma'
+                                    color='orange'
+                                    style={{ marginTop: '10px' }}
+                                  />
+                                </Row>
+                              </>
+                            }
+                            trigger='click'
+                          >
+                            <DeleteOutlined style={{ color: 'red' }} />
+                          </Popover>
+                        </Row>
+                      }
+                    />
+                  </List.Item>
+                )}
               />
-            </Col>
-          </Row>
+            </InfiniteScroll>
+          </div>
         </Col>
       </Row>
       <ParticipantDrawer showDrawer={showParticipantDrawer} closeDrawer={closeParticipantDrawer} />
