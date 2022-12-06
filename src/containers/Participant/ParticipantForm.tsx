30a407aed01da789ca0a59213fae5eb899243a6c
Danilo Caramelo <danilo.caramelo@nomadglobal.com>
2022-12-06 19:18:19 -0300

    feat

--- src/containers/Participant/ParticipantForm.tsx
+++ src/containers/Participant/ParticipantForm.tsx
@@ -1,5 +1,6 @@
 import { Col, DatePicker, Form, Input, Row, Select } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
+import moment from 'moment';
 import { useCallback, useEffect, useState } from 'react';
 import { CustomModal } from '../../components';
 import {
@@ -12,7 +13,6 @@ import {
   Participant,
   updateParticipant,
 } from '../../connections/particpant';
-import { getTeams, Team } from '../../connections/team';
 
 type ParticipantFormProps = {
   visible: boolean;
@@ -40,12 +40,6 @@ export function ParticipantForm({
   const [participantFunctions, setParticipantFunctions] = useState<
     ParticipantFunction[] | undefined
   >([]);
-  const [teams, setTeams] = useState<Team[] | undefined>([]);
-
-  const requestTeams = useCallback(async () => {
-    const response = await getTeams();
-    setTeams(response);
-  }, []);
 
   const requestParticipantFunctions = useCallback(async () => {
     const responseFrameworks = await getParticipantFunctions();
@@ -54,29 +48,41 @@ export function ParticipantForm({
 
   useEffect(() => {
     requestParticipantFunctions();
-    requestTeams();
   }, []);
 
-  const submit = useCallback(async (values: FormValues) => {
-    const finalValues: NewParticipant = {
-      ...values,
-      dtInicioParticipante: values.dtInicioParticipante?.format('YYYY-MM-DD'),
-      dtFimParticipante: values.dtFimParticipante
-        ? values.dtFimParticipante.format('YYYY-MM-DD')
-        : undefined,
-    };
-    console.log(initialValues);
-    if (initialValues) {
-      updateParticipant({ ...finalValues, cdParticipante: String(initialValues.cdParticipante) });
-    } else {
-      await createParticipant(finalValues);
-    }
-    await requestParticipants();
-    closeModal();
-  }, [initialValues]);
+  const submit = useCallback(
+    async (values: FormValues) => {
+      const finalValues: NewParticipant = {
+        ...values,
+        dtInicioParticipante: values.dtInicioParticipante?.format('YYYY-MM-DD'),
+        dtFimParticipante: values.dtFimParticipante
+          ? values.dtFimParticipante.format('YYYY-MM-DD')
+          : undefined,
+      };
+      if (initialValues) {
+        updateParticipant({ ...finalValues, cdParticipante: String(initialValues.cdParticipante) });
+      } else {
+        await createParticipant(finalValues);
+      }
+      await requestParticipants();
+      closeModal();
+    },
+    [initialValues],
+  );
 
   if (initialValues) {
-    form.setFieldsValue(initialValues);
+    form.setFieldValue('nmParticipante', initialValues.nmParticipante);
+    form.setFieldValue('emailParticipante', initialValues.emailParticipante);
+    form.setFieldValue('flParticipante', initialValues.flParticipante);
+    form.setFieldValue(
+      'dtInicioTime',
+      initialValues.dtInicioParticipante ? moment(initialValues.dtInicioParticipante) : undefined,
+    );
+    form.setFieldValue(
+      'dtFinalizacaoTime',
+      initialValues.dtFimParticipante ? moment(initialValues?.dtFimParticipante) : undefined,
+    );
+    form.setFieldValue('cdFuncao', initialValues.funcao.cdFuncao);
   }
 
   return (
@@ -107,25 +113,14 @@ export function ParticipantForm({
             </Form.Item>
           </Col>
         </Row>
-        <Row>
-          <Col span={10}>
-            <Form.Item label='início' name='dtInicioParticipante'>
-              <DatePicker format='DD/MM/YYYY' />
-            </Form.Item>
-            <Form.Item label='fim' name='dtFimParticipante'>
-              <DatePicker format='DD/MM/YYYY' />
-            </Form.Item>
-          </Col>
-          <Col span={13} offset={1}>
-            <Form.Item label='Time' name='time'>
-              <Select>
-                {teams?.map((team) => (
-                  <Select.Option value={team.cdTime} key={team.cdTime}>
-                    {team.nmTime}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
+        <Row justify='space-around'>
+          <Form.Item label='início' name='dtInicioParticipante'>
+            <DatePicker format='DD/MM/YYYY' />
+          </Form.Item>
+          <Form.Item label='fim' name='dtFimParticipante'>
+            <DatePicker format='DD/MM/YYYY' />
+          </Form.Item>
+          <Row>
             <Form.Item label='Função' name='cdFuncao'>
               <Select>
                 {participantFunctions?.map((participantFunction) => (
@@ -138,7 +133,7 @@ export function ParticipantForm({
                 ))}
               </Select>
             </Form.Item>
-          </Col>
+          </Row>
         </Row>
       </>
     </CustomModal>
