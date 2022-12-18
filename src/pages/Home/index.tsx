6da84d37285681d212f4f8ac93e2ee6b0421871e
Danilo Caramelo <danilo.caramelogit@gmail.com>
2022-12-18 11:01:40 -0300

    fix: add alt in Alvo Agil image and removed unused var

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import { Col, Row, Typography } from 'antd';
-import alvoAgilImage from '../../assets/AlvoAgil.png';
 import alvoAgilCompactado from '../../assets/AlvoAgil-compactado.png';
 const { Title } = Typography;
 export function Home() {
   return (
     <Row justify='center' align='middle'>
       <Col>
-        <img src={alvoAgilCompactado} style={{ width: 500, height: 400 }} />
+        <img
+          src={alvoAgilCompactado}
+          style={{ width: 500, height: 400 }}
+          alt='Icone do Alvo Agil com título Alvo Agil'
+        />
         <Title level={4}>Mire na autoavaliação e acerte a produtividade</Title>
       </Col>
     </Row>
